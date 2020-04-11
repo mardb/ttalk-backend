59667acc4084a8047cd4c6790cf20e8981b2eb92
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-04-11 13:32:33 -0700

    setting up mongodb and mongoose

--- server.js
+++ server.js
@@ -2,15 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 require('dotenv').config()
-
+const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/project3';
 
 
 const app = express()
 const PORT = process.env.PORT || 4000
 
-//routs
+// index.js 
+//connect to db
+mongoose.connect(DB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected successfully.'))
+.catch(err => console.lof('MongoDB connection error: ',err))
+
+//routes
 const authRoutes = require('./routes/auth')
 //morgan first  
 app.use(morgan('dev'))
@@ -30,4 +41,4 @@ if(process.env.NODE_ENV = 'development') //change later to localhost
 //   res.send('api page');
 // })
 
-app.listen(PORT, () => console.log(`Server is running at localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Server is running at port: ${PORT}`))
