507f50b1bdad9c0eb9c8edb67643597cf51c3e79
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-04-11 12:37:03 -0700

    adding virtual models and methods for userSchema

--- models/user.js
+++ models/user.js
@@ -1,10 +1,13 @@
+const mongoose = require('mongoose')
+const crypto = require('crypto')
+
 const userSchema = new mongoose.Schema(
   {
       name: {
           type: String,
           trim: true,
           required: true,
-          max: 32
+          max: 50
       },
       email: {
           type: String,
@@ -29,3 +32,39 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+//virtual field
+
+userSchema.virtual('password')
+.set(function(password) {
+  this._password = password
+  this.salt = this.makeSalt()
+  .this.hashed_password = this.encryptPassword(password)
+})
+.get(function() {
+  return this._password
+})
+//methods
+userSchema.methods = {
+  //if thres a match
+  authenticate: function(plainText){
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+
+  encryptPassword:function(password) {
+    if(!password) return ''
+    try {
+      return crypto.createHmac('sha1', this.salt)
+      .update(password)
+      .digest('hex')
+    } catch(err){
+      return ''
+    }
+  },
+  //make salt methond
+  makeSalt: function(){
+    return matchMedia.round(new Date().valueOf() * Math.random()) + ''
+  }
+}
+
+module.exports = mongoose.model('User', userSchema)
