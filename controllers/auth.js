c8ff70f5197535dc3c4e64f173a63a663ecc1001
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-04-11 17:27:46 -0700

    fixing db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,27 +1,32 @@
 const User = require('../models/user')
 
+// exports.signup = (req, res) => {
+//   // console.log('req body for signup', req.body)
+//   const {name, email, password} = req.body
+
+//   User.findOne({email}).exec((err, user) => {
+//     if(user) {
+//       return res.status(400).json({
+//         error: 'Email is already taken'
+//       })
+//     }
+//   })
+//   let newUser = new User({name, email, password})
+
+//   newUser.save((err, success) => {
+//     if(err) {
+//       console.log('Signup Error.', err)
+//       return res.status(400).json({
+//         error: err
+//       });
+//     }
+//     res.json({
+//       message: 'Signup successful! '
+//     });
+//   });
+// };
+
+
 exports.signup = (req, res) => {
-  // console.log('req body for signup', req.body)
-  const {name, email, password} = req.body
-
-  User.findOne({email}).exec((err, user) => {
-    if(user) {
-      return res.status(400).json({
-        error: 'Email is already taken'
-      })
-    }
-  })
-  let newUser = new User({name, email, password})
-
-  newUser.save((err, success) => {
-    if(err) {
-      console.log('Signup Error.', err)
-      return res.status(400).json({
-        error: err
-      });
-    }
-    res.json({
-      message: 'Signup successful! '
-    });
-  });
-};
+  
+}
--- validators/index.js
+++ validators/index.js
@@ -1,7 +1,7 @@
-const{validationResult} = require('express-validator')
+const{validationResult} = require('express-validator');
 
 exports.runValidation = (req, res, next) => {
-  const errors = validationResult(req)
+  const errors = validationResult(req);
   if(!errors.isEmpty()) {
     //422 (Unprocessable Entity)cannot process
     return res.status(422).json({
