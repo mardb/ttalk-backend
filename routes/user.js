084fac8c354e8093425153a5b0b62473c5f054dd
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-04-14 23:24:34 -0700

    FINALLY... fixing a major bug. safely commit, sleep. repeat

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {requireSignin} =  require('../controllers/auth')
+const { requireSignin} =  require('../controllers/auth')
 const { read } = require("../controllers/user");
 
 //validators
 
 //checks toke with requireSignin then reads
-router.get("/user/:id", requireSignin, read );
+router.get("/user/:id",  requireSignin, read );
 
 
 module.exports = router;
+
