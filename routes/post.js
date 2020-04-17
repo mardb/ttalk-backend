32b727c1c4c1d53ce873dafaf7c536f1ae235db5
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-04-17 16:22:18 -0700

    Revert commit: 9487b08

--- routes/post.js
+++ routes/post.js
@@ -2,15 +2,11 @@
 const express = require('express')
 const {getPosts, createPost} = require('../controllers/post')
 const {createPostValidator } = require('../validator/auth')
-// const {requireSignin} = require('../controllers/auth')
-
 
 const router = express.Router()
 
 router.get('/posts',getPosts)
-router.post('/post',  createPostValidator , createPost)
-//with validation
-// router.post('/post', requireSignin, createPostValidator , createPost)
+router.post('/post', requireSignin, createPostValidator , createPost)
 
 module.exports = router;
 
