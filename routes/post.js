5ba10bffef647db8066184597ddcd55e61e28b02
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-04-17 15:23:05 -0700

    safety commit installing node_modules

--- routes/post.js
+++ routes/post.js
@@ -2,11 +2,15 @@
 const express = require('express')
 const {getPosts, createPost} = require('../controllers/post')
 const {createPostValidator } = require('../validator/auth')
+// const {requireSignin} = require('../controllers/auth')
+
 
 const router = express.Router()
 
 router.get('/posts',getPosts)
-router.post('/post', requireSignin, createPostValidator , createPost)
+router.post('/post',  createPostValidator , createPost)
+//with validation
+// router.post('/post', requireSignin, createPostValidator , createPost)
 
 module.exports = router;
 
